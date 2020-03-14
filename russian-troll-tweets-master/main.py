77e2a78cc532b8520362bf65df78221182e4ece0
Benjamin <Beneagar@gmail.com>
2020-03-14 17:10:57 -0600

    Added to the main file to change the data type of publish_date to datatime. I should add this functionallity to the data structure tools evantually. I also plotted a histogram wrt date.

--- russian-troll-tweets-master/main.py
+++ russian-troll-tweets-master/main.py
@@ -2,22 +2,21 @@
 import pandas as pd
 import dataStructureTools
 import matplotlib.pyplot as plt
+import sklearn.cluster
 # %%
 dataStructureTools.mergeData()
 
 # %%
 allData = pd.read_csv('IRAhandle_tweets_all.csv')
+allData['publish_date'] = allData['publish_date'].astype('datetime64[ns]') 
+allData['publish_date'].hist()
+#%%
+subData = allData.sample(frac= 0.001)
+subData['publish_date'] = subData['publish_date'].astype('datetime64[ns]') 
+# %%
+sklearn.cluster.k_means()
+# %%
 
 # %%
-langData = dataStructureTools.splitDFByLanguage()
-
-# %%
-langSum = dict()
-for lang in langData.keys():
-    langSum[lang] = langData[lang].describe(include = 'all')
-
-# %%
-for lang in langSum.keys():
-    langSum[lang].to_excel(lang + ".xlsx")
 
 # %%
