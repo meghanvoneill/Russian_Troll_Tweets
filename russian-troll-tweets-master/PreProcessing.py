781bea9f2d6a9ddb0adab81a7ecf97c503f7b7fe
Meghan V. O'Neill <u0759157@utah.edu>
2020-04-13 20:41:45 -0600

    Preprocessing removes URLs before generating kgrams and features. Currently working on visualize_clustering method, but incomplete.

--- russian-troll-tweets-master/PreProcessing.py
+++ russian-troll-tweets-master/PreProcessing.py
@@ -6,6 +6,7 @@ import numpy as np
 import dataStructureTools
 import sklearn.cluster
 import sklearn.feature_extraction.text as txtvectorizer
+import re
 
 
 # This file includes any function that deals with reading in the raw
@@ -14,7 +15,12 @@ import sklearn.feature_extraction.text as txtvectorizer
 def pre_process(kGram, token_type='word', file_name = 'SubdataSample.csv'):
 
     data = pd.read_csv(file_name, parse_dates = True)
+    # Remove URLs from the data first
+    for t in range(data.shape[0]):
+        data['content'].loc[t] = re.sub(r"http\S+", '', data['content'][t])
     vectorizer = sklearn.feature_extraction.text.TfidfVectorizer(analyzer=token_type,ngram_range=kGram)
     dataMatrix = vectorizer.fit_transform(data['content'])
+    features = (vectorizer.get_feature_names())
+    print("\n\nFeatures : \n", features)
 
-    return data, dataMatrix
+    return data, dataMatrix, features
--- russian-troll-tweets-master/pipeline.py
+++ russian-troll-tweets-master/pipeline.py
@@ -9,18 +9,30 @@ import Mining
 import PostAnalysis
 
 def simple_clustering_pipeline():
-    data, dataMatrix = PreProcessing.pre_process((2,3)) 
+    data, dataMatrix, features = PreProcessing.pre_process((2,3))
     labledData = Mining.SimpleKGram(data,dataMatrix,20)
     labledData.to_csv('simpleClusteringK20.csv')
 
 
-def visualize_clustering():
-    data, dataMatrix = PreProcessing.pre_process((2,3)) 
-    labledData = Mining.SimpleKGram(data,dataMatrix,20)
+def visualize_clustering(clusters):
+    data, dataMatrix, features = PreProcessing.pre_process((2,3))
+    labledData = Mining.SimpleKGram(data,dataMatrix,clusters)
+
+    # Getting top ranking features
+
+
+    # get the first vector out (for the first document)
+    first_vector_tfidfvectorizer = dataMatrix[0]
+
+    # place tf-idf values in a pandas data frame
+    df = pd.DataFrame(first_vector_tfidfvectorizer.T.todense(), index=vectorizer.get_feature_names(),
+                      columns=["tfidf"])
+    df.sort_values(by=["tfidf"], ascending=False)
+    print(df)
 
 
 def clusters_in_two_dim():
-    data, dataMatrix = PreProcessing.pre_process((2,3)) 
+    data, dataMatrix, features = PreProcessing.pre_process((2,3))
     labledData = Mining.SimpleKGram(data,dataMatrix,20)
     labledData = Mining.project_to_two_dimensions(labledData,dataMatrix)
     PostAnalysis.plot_2D(labledData,'2dPlotSimpleClusteringk20.png')
